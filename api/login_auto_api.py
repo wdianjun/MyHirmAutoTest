9e0cb765969816e86c78aaae52257b41307a76d1
D1anJun <wdianjunk@163.com>
2026-06-15 17:17:29 +0800

    添加了allure

--- api/login_auto_api.py
+++ api/login_auto_api.py
@@ -1,13 +1,17 @@
+import allure
 import requests
+
+
 class LoginApi:
-  @classmethod
-  def hirm_login(self,json_data):
-      reqs = requests.post(url='http://ihrm-java.itheima.net/api/sys/login',
-                           json=json_data)
-      return reqs
+    @classmethod
+    @allure.step("POST /api/sys/login")
+    def hirm_login(self, json_data):
+        reqs = requests.post(url='http://ihrm-java.itheima.net/api/sys/login',
+                             json=json_data)
+        return reqs
 
 
 if __name__ == '__main__':
-  json_data = {'mobile': '13800000002', 'password': '929itheima.CN032@.20260614'}
-  res = LoginApi.hirm_login(json_data)
-  print(res.json())
+    json_data = {'mobile': '13800000002', 'password': '929itheima.CN032@.20260614'}
+    res = LoginApi.hirm_login(json_data)
+    print(res.json())
--- scripts/test_login_params_log.py
+++ scripts/test_login_params_log.py
@@ -1,3 +1,4 @@
+import allure
 import logging
 
 import pytest
@@ -9,22 +10,22 @@ from common.read_data import read_login_data
 from config import BASE_DIR
 
 
+@allure.feature("登录模块")
 class TestLogin:
     # 初始化日志
-    init_log_config(BASE_DIR + '/log/hirm_login.log',interval=3,backupCount=7)
+    init_log_config(BASE_DIR + '/log/hirm_login.log', interval=3, backupCount=7)
     # 参数化
     filename = BASE_DIR + '/data/login.json'
-    @pytest.mark.parametrize('desc,req_data,status_code,success,code,message',read_login_data(filename))
-    def test_hirm_login_success(self,desc,req_data,status_code,success,code,message):
-        # json_data = {'mobile': '13800000002', 'password': '929itheima.CN032@.20260612'}
-        resp = LoginApi.hirm_login(req_data)
-        # print(desc + ':',resp.json())
+
+    @allure.story("HRM系统登录")
+    @allure.severity(allure.severity_level.BLOCKER)
+    @pytest.mark.parametrize('desc,req_data,status_code,success,code,message',
+                             read_login_data(filename))
+    def test_hirm_login_success(self, desc, req_data, status_code, success, code, message):
+        allure.dynamic.title(desc)
+        with allure.step("发送登录请求"):
+            resp = LoginApi.hirm_login(req_data)
         # 输出日志
         logging.info(f"{desc}:{resp.json()}")
-        assert_login_common(resp,status_code,success,code,message)
-
-        # 1.对读数据的方法上传文件进行参数化
-        # 2.打印日志
-
-
-
+        with allure.step("校验响应"):
+            assert_login_common(resp, status_code, success, code, message)
